ef9b1f6f5804dd0c373478896566421e7f105939
MarcusFN <113129217+MarcusFN@users.noreply.github.com>
2023-02-20 08:47:10 +0000

    besked

---
+++ fix-samples.sh
@@ -0,0 +1,8 @@
+cd $CODESPACE_VSCODE_FOLDER/samples/
+mv animals/countries/ .
+mv countries/movies/ .
+mv animals/monkies.txt animals/Monkeys.txt
+echo Monkeys > animals/Monkeys.txt
+mv movies/Germany.txt countries
+mv animals/countries .apes.txt
+cd -
